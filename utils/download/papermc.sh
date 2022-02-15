6dcd434bf61e0fcbee084c5a9a0a72c53f3870a3
yude <i@yude.jp>
2022-02-15 13:01:21 +0900

    Add: Download script (PaperMC, Waterfall)

---
+++ utils/download/papermc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Fetch URL
+URL=https://papermc.io/api/v2/projects/paper
+MC_VERSION=$(wget -qO - $URL | jq -r '.versions[-1]')
+URL=${URL}/versions/${MC_VERSION}
+BUILD=$(wget -qO - $URL | jq '.builds[-1]')
+JAR_NAME=paper-${MC_VERSION}-${BUILD}.jar
+URL=${URL}/builds/${BUILD}/downloads/${JAR_NAME}
+
+# Download paper.jar
+wget ${URL} -O paper.jar
---
+++ utils/download/waterfall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Fetch URL
+URL=https://papermc.io/api/v2/projects/waterfall
+MC_VERSION=$(wget -qO - $URL | jq -r '.versions[-1]')
+URL=${URL}/versions/${MC_VERSION}
+BUILD=$(wget -qO - $URL | jq '.builds[-1]')
+JAR_NAME=waterfall-${MC_VERSION}-${BUILD}.jar
+URL=${URL}/builds/${BUILD}/downloads/${JAR_NAME}
+
+# Download paper.jar
+wget ${URL} -O waterfall.jar
