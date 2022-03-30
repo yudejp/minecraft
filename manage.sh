d1fa1c2d446beeed8169cce484f1a904485e6fcb
yudejp <i@yude.jp>
2022-03-30 20:49:18 +0900

    Add help, shell-* command

--- manage.sh
+++ manage.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 # Count the number of arguments
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 (build|up|down|ps|attach-p|attach-w|git-ls)" 1>&2
+  echo "Usage: $0 (build|up|down|ps|attach-p|attach-w|shell-p|shell-w|shell-db|git-ls)";
+  echo "For detailed information, please run '$0 help'." 1>&2
+
   exit 1
 fi
 
 if [ $# = 1 ]; then
     ret=$1
 
-    if [ "$ret" == "up" ]; then
-        docker-compose up -d
+    if [ "$ret" == "help" ]; then
+        echo "Showing the help of $0."
+        echo "The list of available commands:"
+        echo "  * build: Pulls and builds all of containers"
+        echo "  * ps: Shortcut to docker-compose ps"
+        echo "  * up: Start all containers"
+        echo "  * down: Stop all containers"
+        echo "  * attach-p: Attach to PaperMC's console"
+        echo "  * attach-w: Attach to Waterfall's console"
+        echo "  * shell-p: Open the shell of PaperMC container"
+        echo "  * shell-w: Open the shell of Waterfall container"
+        echo "  * shell-db: Open the shell of MariaDB container"
+        
+    elif [ "$ret" == "up" ]; then
+        docker-compose up -d --remove-orphans
     elif [ "$ret" == "down" ]; then
         docker-compose down
     elif [ "$ret" == "attach-p" ]; then
@@ -18,6 +33,15 @@ if [ $# = 1 ]; then
     elif [ "$ret" == "attach-w" ]; then
         WATERFALL_CONTAINER_ID=$(docker-compose ps -q waterfall)
         docker attach $WATERFALL_CONTAINER_ID
+    elif [ "$ret" == "shell-p" ]; then
+        PAPERMC_CONTAINER_ID=$(docker-compose ps -q paper)
+        docker exec -it $PAPERMC_CONTAINER_ID sh
+    elif [ "$ret" == "shell-w" ]; then
+        WATERFALL_CONTAINER_ID=$(docker-compose ps -q waterfall)
+        docker exec -it $WATERFALL_CONTAINER_ID sh
+    elif [ "$ret" == "shell-db" ]; then
+        DB_CONTAINER_ID=$(docker-compose ps -q db)
+        docker exec -it $DB_CONTAINER_ID sh
     elif [ "$ret" == "ps" ]; then
         docker-compose ps
     elif [ "$ret" == "build" ]; then
